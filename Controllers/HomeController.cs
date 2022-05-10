73c2879ea153ecaed4a476837ca5d2c2f71f47ca
zswogger <90625866+zswogger@users.noreply.github.com>
2022-05-10 10:07:14 -0400

    Initial Upload
    Uploading initial project.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WeatherApp.Models;
+using System.Net;
+using Newtonsoft.Json;
+
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Results(string city)
+        {
+            if( city != null)
+            {
+                using (WebClient web = new WebClient())
+                {
+                    string url = string.Format("https://api.openweathermap.org/data/2.5/weather?q={0}&units=imperial&appid={1}", city, "700502a66a4f346702277feff64c0462");
+                    try
+                    {
+                        var json = web.DownloadString(url);
+                        WeatherData.root data = JsonConvert.DeserializeObject<WeatherData.root>(json);
+                        city = char.ToUpper(city[0]) + city.Substring(1);
+                        data.name = city;
+
+                        return View(data);
+                    }
+                    catch(WebException e)
+                    {
+                        // Generally used if a city is misspelled.
+                        Debug.WriteLine("There was an exception. Message: " + e.Message);
+                        return View("Index");
+                    }
+
+                }
+            }
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/WeatherData.cs
@@ -0,0 +1,48 @@
+﻿namespace WeatherApp.Models
+{
+    public class WeatherData
+    {
+        public class coord
+        {
+            public double lon { get; set; }
+            public double lat { get; set; }
+        }
+
+        public class weather
+        {
+            public string main { get; set; }
+            public string description { get; set; }
+            public string icon { get; set; }
+        }
+
+        public class main
+        {
+            public double temp { get; set; }
+            public double temp_min { get; set; }
+            public double temp_max { get; set; }
+            public double humidity { get; set; }
+
+        }
+
+        public class wind
+        {
+            public double speed { get; set; }
+        }
+
+        public class sys
+        {
+            public long sunrise { get; set; }
+            public long sunset { get; set; }
+        }
+
+        public class root
+        {
+            public string name { get; set; }
+            public coord Coord { get; set; }
+            public List<weather> Weather { get; set; }
+            public main Main { get; set; }
+            public wind Wind { get; set; }
+            public sys Sys { get; set; }
+        }
+    }
+}
